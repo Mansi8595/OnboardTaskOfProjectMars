13aaba17795fdca7119a6a5df8361329371eb3c7
Mansi8595 <mansisolanki1995@gmail.com>
2024-10-16 12:06:16 +1300

    update code

--- Pages/LoginPage.cs
+++ Pages/LoginPage.cs
@@ -20,8 +20,8 @@ namespace Mars_SpecFlowProject1.Pages
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
             wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/a")));
             driver.FindElement(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/a")).Click();
-            driver.FindElement(By.Name("email")).SendKeys("mansisolanki1995@gmail.com");
-            driver.FindElement(By.Name("password")).SendKeys("4321@Point");
+            driver.FindElement(By.Name("email")).SendKeys("mansisolanki0805@gmail.com");
+            driver.FindElement(By.Name("password")).SendKeys("Qwerfdsa1@");
             driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[4]/button")).Click();
         }
     }
